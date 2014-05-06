cab2686e9d24c65860a30ad05c5ccd90b30305dd
lucerna <anrecul@gmail.com>
2014-05-06 06:00:28 -0400

    Added script now

---
+++ log4js.js
@@ -0,0 +1,31 @@
+if(!window.console) {
+  alert("No console for browser");
+  console = {};
+}
+
+console.log = console.log || funciton() {};
+console.warn = console.warn || function() {};
+console.error = console.error || function() {};
+console.info = console.info || function() {};
+
+  
+var Logger = (function () {
+  function Logger() {
+    var log = console.log;
+    console.log = function(callback) {
+      log.call(this, arguments[0]);
+      callback.call();
+    }
+  };
+
+  var instance;
+  return {
+    getInstance: function() {
+                   if(instance === null) {
+                    instance = new Logger();
+                    instance.constructor = null;
+                   }
+                  }
+  }
+});
+
