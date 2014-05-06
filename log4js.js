1f1dd94d86e2fc127e1e136e16cfe7328e5cf596
lucerna <anrecul@gmail.com>
2014-05-06 14:34:27 -0400

    Added logging levels.
    Logger now has a static private Level object. This is just a mapping
    from verbose log levels to a value. When logging, if verbose log level
    value is lower than the current level, then we wont log that message.

--- log4js.js
+++ log4js.js
@@ -13,14 +13,46 @@ var Logger = (function () {
   function Logger() {
     //Private logging stuff...
     var log = console.log;
-    console.log = function() {
-      log.call(this, arguments[0]);
-    }
-
+    var LEVEL = new Level();
+    console.log = function(message, callback) {
+      log.call(this, message);
+      if(callback) {
+        callback();
+      } 
+    };
     return {
-      instance.log
+      log: function(message, callback) {
+             message  = typeof message !== 'undefined' ? message : "";
+             callback = typeof callback !== 'undefined' ? callback : null;
+             console.log(message, callback);
+           },
+
+      setLogLevel: function(level) {
+                    LEVEL.setLevel(level);              
+                   }
     }
   };
+  var Level = (function() {
+      function Level() {};
+      var LOG   = 0;
+      var DEBUG = 1;
+      var INFO  = 2;
+      var WARN  = 3;
+      var ERROR = 4;
+
+      var currentLevel = 0;
+
+      return {
+        setLevel: function(level) {
+                    if(Level[level]) {
+                      currentLevel = Level[level];
+                    }
+                  },
+        getLevel: function() {
+                    return currentLevel;
+                  }
+        }
+    });
 
   var instance;
   return {
@@ -29,6 +61,7 @@ var Logger = (function () {
                     instance = new Logger();
                     instance.constructor = null;
                    }
+                   return instance;
                   }
   }
 })();
